e94506abfc961dd478014d41e9fcf58fa798714e
HannaBodnarovska <117848980+HannaBodnarovska@users.noreply.github.com>
2023-06-29 19:55:57 +0200

    дз

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,40 @@
-import { galleryItems } from './gallery-items.js';
-import * as basicLightbox from 'basiclightbox';
-
+const galleryItems = [
+  {
+    preview: 'small-image.jpg',
+    original: 'large-image.jpg',
+    description: 'Image description 1',
+  },
+  {
+    preview: 'small-image.jpg',
+    original: 'large-image.jpg',
+    description: 'Image description 2',
+  },
+  {
+    preview: 'small-image.jpg',
+    original: 'large-image.jpg',
+    description: 'Image description 3',
+  },
+];
 
 const gallery = document.querySelector('.gallery');
-const lightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionsData: 'alt',
-  captionDelay: 250,
-});
 
-const createGalleryItem = ({ preview, original, description }) => {
-  return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          alt="${description}"
-        />
-      </a>
-    </li>
-  `;
+const createGalleryMarkup = (items) => {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
 };
 
-const galleryMarkup = galleryItems
-  .map(createGalleryItem)
-  .join('');
+gallery.innerHTML = createGalleryMarkup(galleryItems);
 
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+});
