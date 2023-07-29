f1450b9a5b132d2b2fab6483ef2e16bf51c5d2be
HannaBodnarovska <117848980+HannaBodnarovska@users.noreply.github.com>
2023-07-29 10:59:20 +0200

    new

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,40 +1,4 @@
-const galleryItems = [
-  {
-    preview: 'small-image.jpg',
-    original: 'large-image.jpg',
-    description: 'Image description 1',
-  },
-  {
-    preview: 'small-image.jpg',
-    original: 'large-image.jpg',
-    description: 'Image description 2',
-  },
-  {
-    preview: 'small-image.jpg',
-    original: 'large-image.jpg',
-    description: 'Image description 3',
-  },
-];
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
-const gallery = document.querySelector('.gallery');
-
-const createGalleryMarkup = (items) => {
-  return items
-    .map(({ preview, original, description }) => {
-      return `
-        <li class="gallery__item">
-          <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
-          </a>
-        </li>
-      `;
-    })
-    .join('');
-};
-
-gallery.innerHTML = createGalleryMarkup(galleryItems);
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionDelay: 250,
-});
+console.log(galleryItems);
